67b7c24e0de7d01268825d345f9e1dca91a86cb3
Astridz3 <112516989+Astridz3@users.noreply.github.com>
2022-09-13 00:01:09 -0600

    Add files via upload

---
+++ Unidad3 Semana4 Clase11 Ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int number = 0;
+	int sum = 0;
+	do {	
+		sum += number;
+		cout << "Ingrese un numero: ";
+		cin >> number;	
+	} 
+	while ( number >= 0 );
+	cout << "\nLa suma es: " << sum << endl;
+		
+		return 0; 
+}
